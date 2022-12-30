daa54390e620152f1afcef7f5e4fd488e9f6653e
maniplek <maniplek81@gmail.com>
2022-12-30 16:21:12 +0200

    validation with Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -9,16 +10,22 @@ class LoginForm extends Component {
 
   //to find an error we can use //errors.find(e=>e.name === 'username) but this can be not clean instead let's use error object we defined in state
 
+  //Joi schema
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   validate = () => {
+    const options = {abortEarly: false}
+    const {error} = Joi.validate(this.state.account, this.schema,options);
+
+    if (!error) return null;
+
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors; // this will return array of all the keys in this object
+    for (let item of error.details) 
+        errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
@@ -32,22 +39,19 @@ class LoginForm extends Component {
     console.log("submited....");
   };
 
-  validateProperty =  ({name, value}) => {
-    if(name === 'username'){
-        if(value.trim() === '') return 'Username is required.'
-        // ... any other validation...
-    }
-    if(name === 'password'){
-        if(value.trim() === '') return 'Password is required.'
-    }
-  }
+  validateProperty = ({ name, value }) => {
+   const obj = { [name]: value};
+   const schema = { [name]: this.schema[name] };
+   const { error } = Joi.validate(obj, schema);
+   return error ? error.details[0].message : null
+  };
 
   handleChange = ({ currentTarget: input }) => {
-   
-    const errors = {...this.state.errors};
-    const errorMessage = this.validateProperty(input) //this is checking error on each input
-    if(errorMessage) errors[input.name] = errorMessage //this is the eeror message we will get in validationProperty function 
-    else delete errors[input.name] //if there is no error in input feild 
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input); //this is checking error on each input
+    if (errorMessage) errors[input.name] = errorMessage;
+    //this is the eeror message we will get in validationProperty function
+    else delete errors[input.name]; //if there is no error in input feild
 
     const account = { ...this.state.account };
     account[input.name] = input.value;
