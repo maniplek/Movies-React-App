1530d084ab20bb86717357bf962deb59c8c9a2e5
maniplek <maniplek81@gmail.com>
2022-12-31 13:55:53 +0200

    added new movie form

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
             Movies
           </NavLink>
           <NavLink className="nav-item nav-link" to="/customers">
-            Customers
+            Customer
           </NavLink>
           <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
